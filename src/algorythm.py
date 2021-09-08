56d8d8c4a5cf1d9711dea3ad1fdbb4ff9351982f
Jaime Andres Torres B <69700780+XaurDesu@users.noreply.github.com>
2021-09-08 15:20:17 -0500

    Add pretty much everything

---
+++ src/algorythm.py
@@ -0,0 +1,192 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+import matplotlib.pyplot as plt
+import os
+
+#Dataset definer
+image_size = (180, 180)
+batch_size = 32
+
+train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    "Dogs_and_cats/data",
+    validation_split=0.2,
+    subset="training",
+    seed=1337,
+    image_size=image_size,
+    batch_size=batch_size,
+)
+val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    "Dogs_and_cats/data",
+    validation_split=0.2,
+    subset="validation",
+    seed=1337,
+    image_size=image_size,
+    batch_size=batch_size,
+)
+data_augmentation = keras.Sequential(
+    [
+        layers.RandomFlip("horizontal"),
+        layers.RandomRotation(0.1),
+    ]
+)
+
+#LOGIC FUNCTIONS
+def remove_corrupted_images():
+    """
+    Removes corrupted and otherwise bad images from the dataset
+    for a better ML training process.
+    """
+    
+
+    num_skipped = 0
+    #Subfolder Tuple
+    for folder_name in ("Cats", "Dogs"):
+        #Sets path to check
+        folder_path = os.path.join("Dogs_and_cats/data", folder_name)
+        for fname in os.listdir(folder_path):
+            fpath = os.path.join(folder_path, fname)
+            try:
+                fobj = open(fpath, "rb")
+                is_jfif = tf.compat.as_bytes("JFIF") in fobj.peek(10)
+            finally:
+                fobj.close()
+
+            if not is_jfif:
+                num_skipped += 1
+                # Delete corrupted image
+                os.remove(fpath)
+
+    print("Imagenes Borradas: %d" % num_skipped)
+
+
+
+def make_model(input_shape, num_classes):
+
+    inputs = keras.Input(shape=input_shape)
+    # Image augmentation block
+    inputs = keras.Input(shape=input_shape)
+    x = data_augmentation(inputs)
+    x = layers.Rescaling(1./255)(x)
+
+    # Entry block
+    x = layers.Rescaling(1.0 / 255)(x)
+    x = layers.Conv2D(32, 3, strides=2, padding="same")(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+
+    x = layers.Conv2D(64, 3, padding="same")(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+
+    previous_block_activation = x  # Set aside residual
+
+    for size in [128, 256, 512, 728]:
+        x = layers.Activation("relu")(x)
+        x = layers.SeparableConv2D(size, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.Activation("relu")(x)
+        x = layers.SeparableConv2D(size, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
+
+        # Project residual
+        residual = layers.Conv2D(size, 1, strides=2, padding="same")(
+            previous_block_activation
+        )
+        x = layers.add([x, residual])  # Add back residual
+        previous_block_activation = x  # Set aside next residual
+
+    x = layers.SeparableConv2D(1024, 3, padding="same")(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+
+    x = layers.GlobalAveragePooling2D()(x)
+    if num_classes == 2:
+        activation = "sigmoid"
+        units = 1
+    else:
+        activation = "softmax"
+        units = num_classes
+
+    x = layers.Dropout(0.5)(x)
+    outputs = layers.Dense(units, activation=activation)(x)
+    return keras.Model(inputs, outputs)
+
+def model_trainer(model):
+    epochs = 50
+
+    callbacks = [
+        keras.callbacks.ModelCheckpoint("save_at_{epoch}.h5"),
+    ]
+    model.compile(
+        optimizer=keras.optimizers.Adam(1e-3),
+        loss="binary_crossentropy",
+        metrics=["accuracy"],
+    )
+    model.fit(
+        train_ds, epochs=epochs, callbacks=callbacks, validation_data=val_ds,
+    )
+
+def image_show():
+    plt.figure(figsize=(10, 10))
+    for images, labels in train_ds.take(1):
+        for i in range(9):
+            ax = plt.subplot(3, 3, i + 1)
+            plt.imshow(images[i].numpy().astype("uint8"))
+            plt.title(int(labels[i]))
+            plt.axis("off")
+
+def add_img():
+    data_augmentation = keras.Sequential(
+    [
+        layers.RandomFlip("horizontal"),
+        layers.RandomRotation(0.1),
+    ]
+    )
+
+def inferencia(model, ruta):
+    img = keras.preprocessing.image.load_img(
+        ruta, target_size=image_size
+    )
+    img_array = keras.preprocessing.image.img_to_array(img)
+    img_array = tf.expand_dims(img_array, 0)  # Create batch axis
+
+    predictions = model.predict(img_array)
+    score = predictions[0]
+    print(
+        "Imagen %.2f por ciento gato y %.2f por ciento perro."
+        % (100 * (1 - score), 100 * score)
+    )
+#MAIN FUNCTIONS
+def main():
+    remove_corrupted_images()
+    
+    model = make_model(input_shape=image_size + (3,), num_classes=2)
+    keras.utils.plot_model(model, show_shapes=True)
+
+    print("Que hacer?")
+    print("1. Ver 9 primeras imagenes del dataset.")
+    print("2. añadir imagenes generadas al modelo")
+    print("3. Entrenar el modelo")
+    print("4. Hacer inferencia en nueva imagen")
+
+    inpt = input()
+    #Switchcase chimbo
+    if inpt == "1":
+        image_show()
+    elif inpt == "2":
+        add_img()
+    elif inpt == "3":
+        model_trainer()
+    elif inpt == "4":
+        print("ingrese la ruta")
+        ruta = input()
+        inferencia(model, ruta)
+
+
+
+if __name__ == "__main__":
+    main()
